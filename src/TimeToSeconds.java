86da3f757f5fe74ea6ee6c84c60736c6b60c8fcd
Anderson Vieira <asvieira.br@gmail.com>
2024-04-04 17:28:11 -0300

    Exercicio 2

---
+++ src/TimeToSeconds.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TimeToSeconds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter hours: ");
+        int hours = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter minutes: ");
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter seconds: ");
+        int seconds = Integer.parseInt(scanner.nextLine());
+
+        scanner.close();
+
+        int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+        System.out.println("Total seconds: " + totalSeconds);
+
+
+    }
+}

db95d63bbb6d347eabbe2b89e11789b914227829
Anderson Vieira <asvieira.br@gmail.com>
2024-04-04 17:27:51 -0300

    Exercicio 1

---
+++ src/CelciusToFahrenheit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CelciusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double celcius, fahrenheit;
+
+        System.out.print("Enter celcius: ");
+        celcius = scanner.nextDouble();
+
+        fahrenheit = (celcius * 1.8) + 32;
+
+        String message = String.format("%.1fºC is %.1fºF", celcius, fahrenheit);
+        System.out.println(message);
+    }
+}

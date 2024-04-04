e649566a1d73249bb08f0a7834f86bfd11c0b54f
Anderson Vieira <asvieira.br@gmail.com>
2024-04-04 17:28:23 -0300

    Exercicio 3

---
+++ src/TipCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double ammount, tip, tax;
+
+        System.out.print("Enter ammount: ");
+        ammount = scanner.nextDouble();
+
+        System.out.print("Enter tax: ");
+        tax = scanner.nextDouble();
+
+        tip = (ammount * tax)/100;
+        System.out.println("Tip is " + tip);
+    }
+}

d56acf06a5542ed5bac87d4b10dc75a6759b7f92
Anderson Vieira <asvieira.br@gmail.com>
2024-04-11 16:59:27 -0300

    Exercicio 14

---
+++ src/WeightedPurchase.java
@@ -0,0 +1,25 @@
+/*
+As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze.
+Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+*/
+
+import java.util.Scanner;
+
+public class WeightedPurchase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double appleUnitNormalValue = 0.30, appleUnitPromotionValue = 0.25, amountPurchase;
+
+        System.out.println("How many apples: ");
+        int totalApples = sc.nextInt();
+
+        if(totalApples >= 12) {
+            amountPurchase = totalApples * appleUnitPromotionValue;
+        } else {
+            amountPurchase = totalApples * appleUnitNormalValue;
+        }
+
+        System.out.println("Amount purchase: " + amountPurchase);
+    }
+}
